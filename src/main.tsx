710cde43dc8cbad0566bbd5a7db489ef223323b7
Gideon Kimutai <gideon.kimutai.kim@gmail.com>
2023-04-04 15:54:21 +0300

    fix: plugins (#6)

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,9 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
 import RootLoader from './components/loaders';
-import './index.css';
 import ReactQueryProvider from './contexts/react-query-context';
 import { Toaster } from 'react-hot-toast';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
